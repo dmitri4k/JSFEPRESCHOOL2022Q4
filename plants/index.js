9ee5d98b7552cab8c8fd5a8f8610db7ac3ec028b
dmitri4k <mdsunknown@gmail.com>
2023-01-11 02:27:15 +0300

    feat: add burger menu

--- plants/index.js
+++ plants/index.js
@@ -1,3 +1,20 @@
-console.log(
-  "Самооценка: 100 баллов\n1. Вёрстка валидная +10\n2. Вёрстка семантическая +20\n3. Вёрстка соответствует макету +48\n4. Требования к css + 12\n5. Интерактивность, реализуемая через css +20\n"
-);
+// Burger handler
+(function () {
+  const burgerItem = document.querySelector('.burger');
+  const menu = document.querySelector('.nav');
+  const menuCloseItem = document.querySelector('.nav-close');
+  const menuLinks = document.querySelectorAll('.nav-link');
+  burgerItem.addEventListener('click', () => {
+      menu.classList.add('nav_active');
+  });
+  menuCloseItem.addEventListener('click', () => {
+      menu.classList.remove('nav_active');
+  });
+  if (window.innerWidth <= 767) {
+      for (let i = 0; i < menuLinks.length; i += 1) {
+          menuLinks[i].addEventListener('click', () => {
+              menu.classList.remove('nav_active');
+          });
+      }
+  }
+}());
